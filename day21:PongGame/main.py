0869207393e6aac813bd1db5282e9ca3986dd34f
matan.kaufman <matankaufman1@gmail.com>
2025-02-22 15:44:27 +0200

    update file-day21

--- day21:PongGame/main.py
+++ day21:PongGame/main.py
@@ -1,23 +1,42 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time
 
-paddle = Turtle()
+
+#paddle = Turtle()
+paddle = Paddle
 screen = Screen()
+ball = Ball()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
 
-# key_move:
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down,"Down")
-
-
-
 # shape:
-paddle.shape("square")
-paddle.shapesize(stretch_wid=6, stretch_len=1)
-paddle.setposition(x=350, y=0)
-paddle.color("white")
+#paddle.shape("square")
+#paddle.penup()
+#paddle.color("white")
+screen.tracer(0)
+#paddle.shapesize(stretch_wid=6, stretch_len=1)
+#paddle.setposition(x=350, y=0)
 
+r_paddle = paddle((350, 0))
+l_paddle = paddle((-350, 0))
+
+#key_move:
+
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up, "W")
+screen.onkey(l_paddle.go_down,"S")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
 
 
--- day21:PongGame/paddle.py
+++ day21:PongGame/paddle.py
@@ -4,12 +4,26 @@ STARTING_POSITION = (0, 0)
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.penup()
-        self.shapesize(stretch_wid=6, stretch_len=1)
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.setposition(position)
 
+   # def move_up(self):
+   #     new_y = self.setposition.ycor() + 20
+   #     self.goto(self.xcor(), new_y)
 
+    #def move_down(self):
+    #    new_y = self.setposition.ycor() - 20
+    #    self.goto(self.xcor(), new_y)
 
+    def go_up(self):
+        new_y =  self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
